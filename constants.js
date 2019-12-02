f0944bb008fff761e0d760d92d5adbf36453b2a2
Nate Bunney <51345525+nbunneysky@users.noreply.github.com>
2019-12-02 08:41:50 -0800

    Added Core Values one more place.

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ import * as d3 from 'd3'
 export type TrackId = 'MOBILE' | 'WEB_CLIENT' | 'FOUNDATIONS' | 'SERVERS' |
   'PROJECT_MANAGEMENT' | 'COMMUNICATION' | 'CRAFT' | 'INITIATIVE' |
   'CAREER_DEVELOPMENT' | 'ORG_DESIGN' | 'WELLBEING' | 'ACCOMPLISHMENT' |
-  'MENTORSHIP' | 'EVANGELISM' | 'RECRUITING' | 'COMMUNITY'
+  'MENTORSHIP' | 'EVANGELISM' | 'RECRUITING' | 'COMMUNITY' | 'CORE_VALUES'
 export type Milestone = 0 | 1 | 2 | 3 | 4 | 5
 
 export type MilestoneMap = {
